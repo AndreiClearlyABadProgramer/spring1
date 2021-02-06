5b0aeec8359b8a246b6c54a20ad1a9cc857576e1
AndreiClearlyABadProgrammer <andrej.dik>
2021-02-06 13:00:34 +0300

    initial commit

---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String catBreed;
+
+    public String getCatBreed() {
+        return catBreed;
+    }
+
+    public void setCatBreed(String catBreed) {
+        this.catBreed = catBreed;
+    }
+}
